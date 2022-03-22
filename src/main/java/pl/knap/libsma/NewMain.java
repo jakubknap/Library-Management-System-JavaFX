7f7d90902a84b7e5a85d0c6f9c6df7a336a0a813
Jakub Knap <93727414+jakubknap@users.noreply.github.com>
2022-03-22 16:32:28 +0100

    Update NewMain.java

--- src/main/java/pl/knap/libsma/NewMain.java
+++ src/main/java/pl/knap/libsma/NewMain.java
@@ -1,5 +1,5 @@
 package pl.knap.libsma;
-// Ta klasa stworzona jest tylko na potrzeby spakowania do fat jara. Był jakiś problem z shadowem, a to okazało się pomyślnym obejściem tego problemu.
+// This class is for fat jars only. There was a shadow problem and this turned out to be a successful workaround for this problem.
 public class NewMain {
     public static void main(String[] args) {
         Main.main(args);
